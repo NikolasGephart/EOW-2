ad30b9214279460f123b7853e24f8ec7df941b71
ChyaBoiNik <nikolas.gephart@gmail.com>
2021-07-13 08:06:32 -0600

    implementing js

--- game.js
+++ game.js
@@ -1,6 +1,30 @@
-let cookies = 0;
-
-function mine() {
-  cookies++
-  alert("Cookie")
+var upgrades = {
+  employee: {
+    cps: 0.1,
+    cost: 10
+  },
+  Oven: {
+    clickModifier: 1,
+    cost: 25
+  }
 }
+
+
+
+
+let cookies = 0;
+function mine() {
+
+  cookies++
+  return console.log("Cookie")
+
+}
+
+document.getElementById("cookiePic").addEventListener("click", mine);
+
+
+function test() {
+  return console.log("That Works")
+}
+
+document.getElementById('ug').addEventListener('click', test)
